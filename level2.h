7b8d35fe7363bd5ebf624bde73555017e7a94c0b
WangJun <1103870790@qq.com>
2017-07-26 20:17:33 +0800

    Add config file

--- level2.h
+++ level2.h
@@ -3,7 +3,7 @@
 
 void sgemv(char trans,int m,int n,float alpha,float *a,int lda,float *x,int incx,float beta,float *y,int incy )
 {
-
+    int a_col;
 }
 
 #endif // LEVEL2_H
--- types.h
+++ types.h
@@ -1,6 +1,9 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include"config.h"
+#include"compiler.h"
+
 struct complexf
 {
     float real;
@@ -11,5 +14,26 @@ struct complexd
     double real;
     double imag;
 };
+
+
+#if defined(TINYBLAS_DEFAULT_TO_COL_MAJOR)
+TINYBLAS_INLINE int mindex(int lda,int row,int col)
+{
+    return col*lda+row;
+}
+#elif defined(TINYBLAS_DEFAULT_TO_ROW_MAJOR)
+
+TINYBLAS_INLINE int mindex(int lda,int row,int col)
+{
+    return row*lda+col;
+}
+#else
+TINYBLAS_INLINE int mindex(int lda,int row,int col)
+{
+    return col*lda+row;
+}
+#endif
+
+
 
 #endif // TYPES_H
