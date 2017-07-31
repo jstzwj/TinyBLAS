d8068a3e749525b0ce16f9b5e618ee203820bf8f
WangJun <1103870790@qq.com>
2017-07-31 23:46:27 +0800

    Add the definition of sgemm

--- level3.h
+++ level3.h
@@ -1,4 +1,13 @@
 #ifndef LEVEL3_H
 #define LEVEL3_H
 
+void sgemm(char transa,char transb,
+           int m,int n,int k,
+           float alpha,float *a,int lda,float *b,int ldb,float beta,float *c,int ldc)
+{
+
+}
+
+
+
 #endif // LEVEL3_H
